088a9c2ce60d8d231464323fc761d7d07ffa2622
wani <oen142@naver.com>
2021-04-15 10:02:45 +0900

    Initial commit

---
+++ src/test/java/com/wani/workreport/WorkReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.workreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
